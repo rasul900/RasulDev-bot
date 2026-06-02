b9986f6fbb4e6887f93c42ed39cbbf2b8c64e40e
abdurasul <abdurasulxayrulayev@gmail.com>
2026-06-02 15:17:57 +0500

    AdminID qoshildi

--- handlers/admin.js
+++ handlers/admin.js
@@ -10,5 +10,17 @@ export const adminPanelHandler = async (ctx) => {
   await ctx.reply(
     "⚙️ Admin Panel",
     adminMenu
+  );export const adminPanelHandler = async (ctx) => {
+
+  const adminId = Number(process.env.ADMIN_ID);
+
+  if (ctx.from.id !== adminId) {
+    return ctx.reply("⛔ Siz admin emassiz");
+  }
+
+  await ctx.reply(
+    "⚙️ Admin Panel",
+    adminMenu
   );
+};
 };
