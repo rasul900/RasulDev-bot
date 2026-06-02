666cb00529162995fb6a4b3cfbb4a60907194ac4
abdurasul <abdurasulxayrulayev@gmail.com>
2026-06-02 15:21:20 +0500

    xatolik togirlandi

--- handlers/admin.js
+++ handlers/admin.js
@@ -1,16 +1,4 @@
-import { adminMenu } from "../keyboards/adminMenu.js";
-import { isAdmin } from "../middlewares/isAdmin.js";
-
 export const adminPanelHandler = async (ctx) => {
-
-  if (!isAdmin(ctx)) {
-    return ctx.reply("⛔ Siz admin emassiz");
-  }
-
-  await ctx.reply(
-    "⚙️ Admin Panel",
-    adminMenu
-  );export const adminPanelHandler = async (ctx) => {
 
   const adminId = Number(process.env.ADMIN_ID);
 
@@ -22,5 +10,4 @@ export const adminPanelHandler = async (ctx) => {
     "⚙️ Admin Panel",
     adminMenu
   );
-};
 };
